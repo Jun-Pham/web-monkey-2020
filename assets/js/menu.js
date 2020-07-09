45eea540a5fb744867fced5e041dceb6d7e23e0d
JunPham <long.phamptit@gmail.com>
2020-07-10 05:53:30 +0700

    10/7 update ui home page

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,16 +1,21 @@
 const listMonkeyNavItem = document.querySelectorAll(".monkey-nav-item");
+const isMobile = /android|webos|iphone|ipod|iPad|blackberry|iemobile|opera mini/i.test(
+  navigator.userAgent.toLowerCase()
+);
 for (let i = 0; i < listMonkeyNavItem.length; i++) {
-  listMonkeyNavItem[i].addEventListener("click", function(e){
-    e.stopPropagation();
-    if (this.classList.contains("active")) {
-      this.classList.remove("active");
-    } else {
-      for (let i = 0; i < listMonkeyNavItem.length; i++) {
-        listMonkeyNavItem[i].classList.remove("active");
+  if (isMobile) {
+    listMonkeyNavItem[i].addEventListener("click", function (e) {
+      e.stopPropagation();
+      if (this.classList.contains("active")) {
+        this.classList.remove("active");
+      } else {
+        for (let i = 0; i < listMonkeyNavItem.length; i++) {
+          listMonkeyNavItem[i].classList.remove("active");
+        }
+        this.classList.add("active");
       }
-      this.classList.add("active");
-    }
-  });
+    });
+  }
 }
 
 function onCloseDropdown() {
@@ -23,7 +28,7 @@ function onCloseDropdown() {
 }
 
 window.onclick = (event) => {
-  if (!event.target.matches(".nav-link")) {
+  if (!event.target.matches(".nav-link") && isMobile) {
     onCloseDropdown();
   }
 };
@@ -31,14 +36,13 @@ window.onclick = (event) => {
 const listLanguage = document.querySelectorAll(".item-language");
 const eleIdLang = document.getElementById("icon-lang");
 for (let i = 0; i < listLanguage.length; i++) {
-  listLanguage[i].addEventListener("click", function(e){
+  listLanguage[i].addEventListener("click", function (e) {
     e.stopPropagation();
     const urlImage = this.children[0].children[0].getAttribute("src");
     eleIdLang.src = urlImage;
     onCloseDropdown();
   });
 }
-
 
 $(".toggler-icon").click(function () {
   $(".toggler-icon").toggleClass("active");
@@ -50,24 +54,21 @@ function onShowContentDropdown(id) {
   elmDropdown.classList.toggle("active");
 }
 
-const listContents = document.querySelectorAll(".content-item"); 
+const listContents = document.querySelectorAll(".content-item");
 for (let i = 0; i < listContents.length; i++) {
-  listContents[i].addEventListener("click", function(e){
+  listContents[i].addEventListener("click", function (e) {
     e.stopPropagation();
     e.preventDefault();
     const innerText = this.innerText;
-    const elmInputSupportContent = document.getElementById(
-      "support-content"
-    );
+    const elmInputSupportContent = document.getElementById("support-content");
     elmInputSupportContent.value = innerText;
-    onShowContentDropdown('content-desktop') 
-  }) 
+    onShowContentDropdown("content-desktop");
+  });
 }
 
-
-const listContentsMobile = document.querySelectorAll(".content-item-mobile"); 
+const listContentsMobile = document.querySelectorAll(".content-item-mobile");
 for (let i = 0; i < listContentsMobile.length; i++) {
-  listContentsMobile[i].addEventListener("click", function(e){
+  listContentsMobile[i].addEventListener("click", function (e) {
     e.stopPropagation();
     e.preventDefault();
     const innerText = this.innerText;
@@ -75,6 +76,6 @@ for (let i = 0; i < listContentsMobile.length; i++) {
       "support-content-mobile"
     );
     elmInputSupportContent.value = innerText;
-    onShowContentDropdown('content-mobile')
-  }) 
+    onShowContentDropdown("content-mobile");
+  });
 }
